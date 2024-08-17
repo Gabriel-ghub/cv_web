c2af826d541c2fe93ab7ddb962bf8ea5ab274855
Gabriel Romero <romerogabriel.ad@gmail.com>
2024-08-17 16:31:35 +0200

    Agrego data para poner informacion, cv en pdf y cambios en navbars

---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+export const navItems = [
+  {
+    text: "Experiencia",
+    href: "#experience",
+  },
+  {
+    text: "Tecnologías",
+    href: "#technologies",
+  },
+  {
+    text: "Formación",
+    href: "#education",
+  },
+  {
+    text: "Sobre mí",
+    href: "#aboutme",
+  },
+  {
+    text: "¿Hablamos?",
+    href: "#contact",
+  },
+  {
+    text: "Currículum",
+    href: "CV_Gabriel_Romero.pdf",
+    icon: "pdf",
+  },
+];
+
+export const experience = [
+  {
+    position: "Desarrollador front-end",
+    company: "ADR Formación",
+    dates: "Marzo 2022 - Actualidad",
+    tasks: [
+      "Mantenimiento web.",
+      "Desarrollo responsive.",
+      "Soporte en SEO técnico.",
+      "Desarrollo web con React JS.",
+      "Soporte a back-end con PHP y MySQL.",
+    ],
+  },
+];
