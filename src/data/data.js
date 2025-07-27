c3bb9338bb360c07aaff124c3c64f6c21246d5b8
Gabriel Romero <romerogabriel.ad@gmail.com>
2025-07-27 16:43:57 +0200

    Add hiberus experience

--- src/data/data.js
+++ src/data/data.js
@@ -1,54 +1,25 @@
 export const navItems = [
   {
-    text: "Sobre mí",
-    href: "/",
-  },
-  {
-    text: "Blog",
-    href: "/blog",
-  },
-  // {
-  //   text: "Currículum",
-  //   href: "/CV_Gabriel_Romero_desarrollador_ReactJS.pdf",
-  //   icon: "pdf",
-  //   newTab: true,
-  // },
-];
-
-// {
-//   text: "Blog",
-//   href: "https://gabrielromero.dev/blog/",
-// },
-
-export const experience = [
-  {
-    position: "Desarrollador front-end",
-    company: "ADR Formación",
-    dates: "Marzo 2022 - Actualidad",
-    tasks: [
-      "Mantenimiento web.",
-      "Desarrollo responsive.",
-      "Soporte en SEO técnico.",
-      "Desarrollo web con React JS.",
-      "Soporte a back-end con PHP y MySQL.",
-    ],
+    text: 'Sobre mí',
+    href: '/#aboutme',
   },
 ];
+
 
 export const languages = [
   {
-    name: "javascript",
+    name: 'javascript',
     description: [
-      "Javascript, ese lenguaje pensando para dotar de interactividad a las páginas web que ha ido evolucionando hasta su uso en servidores. El primer lenguaje de programación de muchos.",
+      'Javascript, ese lenguaje pensando para dotar de interactividad a las páginas web que ha ido evolucionando hasta su uso en servidores. El primer lenguaje de programación de muchos.',
     ],
   },
   {
-    name: "docker",
+    name: 'docker',
     description: [
-      "Cuando descubrí Docker, me voló la cabeza.",
-      "Es una herramienta que te permite crear contenedores, como cápsulas, donde puedes meter toda tu app y sus dependencias para que funcione igual en cualquier entorno.",
+      'Cuando descubrí Docker, me voló la cabeza.',
+      'Es una herramienta que te permite crear contenedores, como cápsulas, donde puedes meter toda tu app y sus dependencias para que funcione igual en cualquier entorno.',
       'Olvídate del “en mi máquina funciona".',
-      "Lo uso principalmente para montar entornos de desarrollo súper rápido y evitar problemas de compatibilidad.",
+      'Lo uso principalmente para montar entornos de desarrollo súper rápido y evitar problemas de compatibilidad.',
     ],
   },
 ];
